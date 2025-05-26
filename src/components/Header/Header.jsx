db140edc46340659d3fe70be4fb47fe6ba6e5b06
AlexCleberLimaFerreira <alexcleberlimaferreira@gmail.com>
2025-05-26 18:45:28 -0300

    drip store

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from '../Logo/Logo';
+import './Header.css';
+
+export default function Header() {
+ return (
+    <header>
+      <Logo src={'../../../public/Logo.svg'} cor={'#c92061'}/>
+    </header>
+ ) 
+}
