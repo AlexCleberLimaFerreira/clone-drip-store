4fcd2b95d70c3c0c475f53da37e2b99e0ffe8be8
AlexCleberLimaFerreira <alexcleberlimaferreira@gmail.com>
2025-05-27 13:44:34 -0300

    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,12 @@ import './Header.css';
 
 export default function Header() {
  return (
-    <header>
+    <header id='header'>
+
+      <div className='box-header'>
       <Logo src={'../../../public/Logo.svg'} cor={'#c92061'}/>
+      </div>
+
     </header>
  ) 
 }
